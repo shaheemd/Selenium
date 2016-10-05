4f6017bbd7ca672569c2d627bfd1755fd9cb1dc3
Shame <Shame@192.168.1.6>
2016-10-05 21:40:56 +1300

    Created a locateElements.java

---
+++ src/locateElements.java
@@ -0,0 +1,64 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class locateElements {
+
+	public static void main(String[] args) {
+		/*Setting up a page*/
+		String url = "https://invencothick-nz-test.tms.invenco.com/";
+		//String url = "http://gmail.com/";
+		
+		
+		/*Handler to the firefox browser*/
+		WebDriver driver = new FirefoxDriver();
+		driver.get(url);
+		
+		
+		/*Login Page - https://invencothick-nz-test.tms.invenco.com/
+		 * username - text box
+		 * password - text box
+		 * log in - button
+		 * links - forget password, Privacy Policy, Terms, Help 
+		 * 
+		 * 
+		 * */
+		
+		
+		
+		
+		
+		
+		/*Class*/
+		
+		
+		
+		/*Name*/
+		
+		
+		
+		/*CSS Selector*/
+		
+		
+		
+		/*Xpath*/
+		
+		
+		
+		/*ID*/
+		
+		
+		
+		
+		/*Partial link text*/
+		
+		
+		
+		/*Linked text*/
+		
+		
+		/*Tag Name*/
+		
+		
+	}
+
+}
